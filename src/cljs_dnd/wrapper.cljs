623cdeab608827157795f3a96f0564f077faa1eb
Oleg Akbarov <olegakbarov@gmail.com>
2016-09-18 22:59:49 +0300

    wrapper outline

---
+++ src/cljs_dnd/wrapper.cljs
@@ -0,0 +1,16 @@
+; (defn draggable []
+;   (reagent/create-class {:reagent-render draggable-render
+;                          :component-did-mount draggable-did-mount}))
+
+
+(defn drag-source [type comp target conn-fn]
+  "HOC that registers components as source of drag-start event
+  @type is enum
+  @comp is component to decorate
+  @conn-fn connects to drop target")
+
+(defn drop-target [type comp source conn-fn]
+  "HOC that registers components as target to on-drop event
+  @type is enum
+  @comp is component to decorate
+  @conn-fn accepts @connect & @monitor")
