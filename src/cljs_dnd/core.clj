8ec3d528d2c25f61f283c1f3a0836e4f21310e8f
Oleg Akbarov <olegakbarov@gmail.com>
2016-09-19 22:26:59 +0300

    extracted common logic

--- src/cljs_dnd/core.clj
+++ src/cljs_dnd/core.clj
@@ -2,63 +2,81 @@
 (ns cljs-dnd.core
   (:require-macros [cljs.core.async.macros :refer [go go-loop]])
   (:require
-    [cljs-dnd.state :refer [state]]
+    [cljs-dnd.state :refer [state update-state]]
     [cljs.core.async :refer [<! put! chan timeout]]
-    [goog.dom :as g-dom]
     [goog.events :as events])
   (:import [goog.events EventType]))
 
-
-(defn update-state [key val]
-  (let [keyz (set (keys @state))]
-    (if (contains? keyz key)
-      (swap! state assoc key val)
-      ("ERROR"))))
-
 (defn parse-event [e]
-  {:hix (-> e .-target .-dataset .-index int) ;; index of hovering or hoverable item?
-   :dix (:dix @state)         ;; and here?
+  ; (cond = (-> e .-type)
+  ;   "dragenter" (.preventDefault e)
+  ;   "dragleave" (.preventDefault e))
+  (let [ix (-> e .-target .-dataset .-index int)
+        dix (if (nil? (:dix @state)) ix (:dix @state))
+        bottom (-> e .-target .getBoundingClientRect .-bottom)
+        top (-> e .-target .getBoundingClientRect .-top)
+        event-type (-> e .-type)]
+    {:dix dix
+     :hix ix
+     :top top
+     :bottom bottom
+     :mid (/ (- bottom top) 2)
+     :item-type  (-> e .-target .-dataset .-type)
+     :event-type event-type}))
 
-   :item-bottom (-> e .-target .getBoundingClientRect .-bottom)
-   :item-top (-> e .-target .getBoundingClientRect .-top)
-   :item-mid (/ (- brect-bottom brect-top) 2)
-   :item-type  (-> e .-target .-dataset .-type)
-   :event-type (-> e .-type)}) ;; TODO check!
+(defn handle-drag-start [e]
+  (let [{:keys [dix item-type]} e]))
 
-(defn on-drag-start [e]
-  (let [{:keys [index drag-index brect-top brect-bottom middle-y item-type event-type]} (parse-event e)]))
-    ;; (swap! dnd-store assoc :drag-id index)))
+(defn handle-drag-enter [e]
+  (let [{:keys [dix hix]} @state]))
 
-(defn on-drag-over [e]
-  (let [{:keys [index drag-index brect-top brect-bottom middle-y item-type event-type]} (parse-event e)]))
-    ;; (put! dnd-chan)))
+(defn handle-drag-leave [e]
+  (let [{:keys [dix hix]} @state]))
 
-(defn on-drag [e]
-  "nimp")
-  ;; update client-y
+(defn handle-drag-end [e]
+  (let [{:keys [dix hix]} @state]))
 
-(defn on-drag-end [e]
-  "nimp")
+(defn handle-drag [e]
+  (swap! state assoc :y (.-clientY e)))
+
+(defn handle-drag-over [e]
+  (let [{:keys [dix hix]} @state]))
+
+(defn handle-drop [e]
+  (let [{:keys [dix hix]} @state]))
 
 ; (defn- events->chan [el event-type c]
 ;   (events/listen el event-type #(put! c %))
 ;   c)
 
+;; --------------------------
+;; EVENTS:
+
+(defn on-event [e]
+  (put! dnd-chan (parse-event e)))
+
+;; --------------------------
+;; LISTENER:
+
 (defn listen! []
-  (events/listen js/window EventType.DRAGSTART on-drag-start)
-  (events/listen js/window EventType.DRAGOVER on-drag-over)
-  (events/listen js/window EventType.DRAGEND on-drag-end)
-  ; (events/listen js/window EventType.DRAGENTER on-drag-enter)
-  ; (events/listen js/window EventType.DROP on-drop)
+  (events/listen js/window EventType.DRAGSTART on-event)
+  (events/listen js/window EventType.DRAGOVER  on-event)
+  (events/listen js/window EventType.DRAGEND   on-event)
+  (events/listen js/window EventType.DRAG      on-event)
+  (events/listen js/window EventType.DRAGENTER on-event)
+  (events/listen js/window EventType.DRAGLEAVE on-event)
 
   (go-loop []
      (let [e (<! dnd-chan)
-           {:keys [hix cli-y]} e        ;; destruct event
-           {:keys [drag-index]} @state]  ;; destruct state
-
-        (when-not (= dix hix)
-          (dispatch [:reorder_msg drag-index hover client-y]))
-
+           {:keys [event-type]} e]
+        (condp = event-type
+          "dragenter" (handle-drag-enter e)
+          "dragleave" (handle-drag-leave e)
+          "dragstart" (handle-drag-start e)
+          "dragover"  (handle-drag-over e)
+          "drag"      (handle-drag e)
+          "drop"      (handle-drop e)
+          :default    (prn event-type))
        (recur))))
 
 (listen!)
