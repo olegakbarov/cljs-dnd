743768f5a8b5cb292129daff96b64cc3fc8dbbb0
Oleg Akbarov <olegakbarov@gmail.com>
2016-09-19 22:27:40 +0300

    state accessor

--- src/cljs_dnd/state.cljs
+++ src/cljs_dnd/state.cljs
@@ -4,3 +4,9 @@
 (def state (atom {:dix nil       ;; drag-index
                   :hix nil       ;; hover-index
                   :cli-y nil}))  ;; clientY
+
+(defn update-state [key val]
+  (let [keyz (set (keys @state))]
+    (if (contains? keyz key)
+      (swap! state assoc key val)
+      ("ERROR"))))
