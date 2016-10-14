de29f88b3bf2278fb34a3a3d1e098f7bd98813b4
Oleg Akbarov <olegakbarov@gmail.com>
2016-10-14 22:38:06 +0300

    refactored core

--- src/cljs_dnd/core.clj
+++ src/cljs_dnd/core.clj
@@ -2,55 +2,57 @@
 (ns cljs-dnd.core
   (:require-macros [cljs.core.async.macros :refer [go go-loop]])
   (:require
-    [cljs-dnd.state :refer [state update-state]]
+    [cljs-dnd.state :refer [state]]
     [cljs.core.async :refer [<! put! chan timeout]]
     [goog.events :as events])
   (:import [goog.events EventType]))
 
-(defn parse-event [e]
-  ; (cond = (-> e .-type)
-  ;   "dragenter" (.preventDefault e)
-  ;   "dragleave" (.preventDefault e))
-  (let [ix (-> e .-target .-dataset .-index int)
-        dix (if (nil? (:dix @state)) ix (:dix @state))
-        bottom (-> e .-target .getBoundingClientRect .-bottom)
-        top (-> e .-target .getBoundingClientRect .-top)
-        event-type (-> e .-type)]
-    {:dix dix
-     :hix ix
-     :top top
-     :bottom bottom
-     :mid (/ (- bottom top) 2)
-     :item-type  (-> e .-target .-dataset .-type)
-     :event-type event-type}))
+;; --------------------------
+;; HANDLERS:
 
 (defn handle-drag-start [e]
-  (let [{:keys [dix item-type]} e]))
+  (let [{:keys [drag-index item-type]} e
+        updated {:drag-index drag-index
+                 :item-type item-type}]
+      (swap! state merge updated)))
 
 (defn handle-drag-enter [e]
   (let [{:keys [dix hix]} @state]))
+  ;; TODO update accrordin to event
 
 (defn handle-drag-leave [e]
   (let [{:keys [dix hix]} @state]))
+  ;; TODO update accrordin to event
 
 (defn handle-drag-end [e]
   (let [{:keys [dix hix]} @state]))
+  ;; TODO update accrordin to event
 
 (defn handle-drag [e]
   (swap! state assoc :y (.-clientY e)))
+  ;; TODO update accrordin to event
 
 (defn handle-drag-over [e]
   (let [{:keys [dix hix]} @state]))
+  ;; TODO update accrordin to event
 
-(defn handle-drop [e]
-  (let [{:keys [dix hix]} @state]))
-
-; (defn- events->chan [el event-type c]
-;   (events/listen el event-type #(put! c %))
-;   c)
+(defn handle-drop [e])
+  ;; TODO update accrordin to event
 
 ;; --------------------------
 ;; EVENTS:
+
+(defn parse-event
+  "Normalizes the event for dnd purposes"
+  [e]
+  (let [event-type (-> e .-type)
+        item-index (-> e .-target .-dataset .-index int)
+        bottom (-> e .-target .getBoundingClientRect .-bottom)
+        top (-> e .-target .getBoundingClientRect .-top)]
+    {:event-type event-type
+     :item-index item-index
+     :top top
+     :bottom bottom}))
 
 (defn on-event [e]
   (put! dnd-chan (parse-event e)))
@@ -58,7 +60,7 @@
 ;; --------------------------
 ;; LISTENER:
 
-(defn listen! []
+(defn dnd-start! []
   (events/listen js/window EventType.DRAGSTART on-event)
   (events/listen js/window EventType.DRAGOVER  on-event)
   (events/listen js/window EventType.DRAGEND   on-event)
@@ -78,5 +80,3 @@
           "drop"      (handle-drop e)
           :default    (prn event-type))
        (recur))))
-
-(listen!)
